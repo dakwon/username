4247119b4d53b8130d9d59a4d2b4c7f04e1f194f
Daesik Kwon <dakwon@chapman.edu>
2019-11-07 14:35:20 -0800

    Adds username.sh

---
+++ username.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+# username.sh
+# author
+echo "A valid username must have only lower case letters and digits."
+echo "You can include the underscore character."
+echo "It must start with a lower case letter."
+echo "Your username must include at least three but no more than 12 characters."
+echo "Enter a username: "
+read USERNAME
+while echo "$USERNAME" | egrep -v "^[a-z][a-z0-9_]{2,11}$" > /dev/null 2>&1
+do
+	echo "A valid username must have only lower case letters and digits."
+	echo "You can include the underscore character."
+	echo "It must start with a lower case letter."
+	echo "Your username must include at least three but no more than 12 characters."
+	echo "You must enter a valid username."
+	echo "Enter a username: "
+	read USERNAME
+done
+echo "Thank you"
